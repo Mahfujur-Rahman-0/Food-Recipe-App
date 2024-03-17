538cf9933dd92da7d31c4d1f9f5e8b267a15894b
Mahfujur Rahman <106644950+Mahfujur-Rahman-0@users.noreply.github.com>
2024-03-17 19:30:12 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ function App() {
   const [errorr, setError] = useState(null);
   const url = "https://api.spoonacular.com/recipes/complexSearch";
   const detailsUrl = `https://api.spoonacular.com/recipes/${foodId}/information`;
-  //const apikeey = "b4ff9213d30741c58ded7e68610a0596";
-  const apikeey = "7101900a2282406b9ff4351095d2477f";
+  const apikeey = "b4ff9213d30741c58ded7e68610a0596";
+  //const apikeey = "7101900a2282406b9ff4351095d2477f";
 
   return (
     <>
